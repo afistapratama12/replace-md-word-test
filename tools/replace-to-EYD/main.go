47819af41ed717f72ad49fe4bcb256a0dc2d021d
afistapratama12 <pratama.otori.12@gmail.com>
2022-09-05 09:54:41 +0700

    feat: change lint word

--- tools/replace-to-EYD/main.go
+++ tools/replace-to-EYD/main.go
@@ -1,70 +1,87 @@
 package main
 
 import (
-	"fmt"
 	"io/ioutil"
+	"log"
 	"os"
 	"os/exec"
+	"path/filepath"
 	"strings"
 )
 
-var (
-	start   = []string{"disini", "disana", "disitu", "didalam", "diatas", "dibawah"}
-	replace = []string{"di sini", "di sana", "di situ", "di dalam", "di atas", "di bawah"}
-)
+type ReplacerWord struct {
+	Start    string
+	Replacer string
+}
+
+var listReplacerWord = []ReplacerWord{{"diantara", "di antara"}, {"diakhir", "di akhir"}, {"diatas", "di atas"}, {"diawal", "di awal"}, {"dibawah", "di bawah"}, {"dibelakang", "di belakang"}, {"didalam", "di dalam"}, {"didekat", "di dekat"}, {"didepan", "di depan"}, {"dikanan", "di kanan"}, {"dikiri", "di kiri"}, {"diluar", "di luar"}, {"dimana", "di mana"}, {"disamping", "di samping"}, {"disaat", "di saat"}, {"disana", "di sana"}, {"disebelah", "di sebelah"}, {"disini", "di sini"}, {"disisi", "di sisi"}, {"disitu", "di situ"}, {"ditengah", "di tengah"}, {"ditengah-tengah", "di tengah-tengah"}, {"ditiap", "di tiap"}, {"ditiap-tiap", "di tiap-tiap"},
+	{"Diantara", "Di antara"}, {"Diakhir", "Di akhir"}, {"Diatas", "Di atas"}, {"Diawal", "Di awal"}, {"Dibawah", "Di bawah"}, {"Dibelakang", "Di belakang"}, {"Didalam", "Di dalam"}, {"Didekat", "Di dekat"}, {"Didepan", "Di depan"}, {"Dikanan", "Di kanan"}, {"Dikiri", "Di kiri"}, {"Diluar", "Di luar"}, {"Dimana", "Di mana"}, {"Disamping", "Di samping"}, {"Disaat", "Di saat"}, {"Disana", "Di sana"}, {"Disebelah", "Di sebelah"}, {"Disini", "Di sini"}, {"Disisi", "Di sisi"}, {"Disitu", "Di situ"}, {"Ditengah", "Di tengah"}, {"Ditengah-tengah", "Di tengah-tengah"}, {"Ditiap", "Di tiap"}, {"Ditiap-tiap", "Di tiap-tiap"},
+}
 
 func main() {
 	out, err := exec.Command("git", "diff", "--name-only", "HEAD^").Output()
 	if err != nil {
+		log.Println("Error exec command", err.Error())
 		panic(err)
 	}
 
-	data := string(out)
+	listFileName := string(out)
 
-	for _, line := range strings.Split(data, "\n") {
-		if line != "" && line[len(line)-3:] == ".md" {
-			// res := OpenFile(line)
-			// newData := ReplaceToEYD(res)
-			// WriteNewFile(newData, line)
-
-			fmt.Println(line)
+	for _, fileName := range strings.Split(listFileName, "\n") {
+		if fileName != "" && filepath.Ext(fileName) == ".md" {
+			MDText, err := OpenFile(fileName)
+			if err != nil {
+				log.Println("Error open file", err.Error())
+				panic(err)
+			}
+			newMDText := ReplaceToEYD(MDText)
+			err = OverwriteText(newMDText, fileName)
+			if err != nil {
+				log.Println("Error Overwrite text file", err.Error())
+				panic(err)
+			}
 		}
 	}
 }
 
-func ReplaceToEYD(data string) string {
-	for i := 0; i < len(start); i++ {
-		data = strings.Replace(data, start[i], replace[i], -1)
+func ReplaceToEYD(textFile string) string {
+	for i := 0; i < len(listReplacerWord); i++ {
+		textFile = strings.Replace(textFile, listReplacerWord[i].Start, listReplacerWord[i].Replacer, -1)
 	}
 
-	return data
+	return textFile
 }
 
-func OpenFile(path string) string {
+func OpenFile(path string) (string, error) {
 	f, err := os.Open(path)
 	if err != nil {
-		panic(err)
+		return "", err
 	}
 
 	defer f.Close()
 
 	b, err := ioutil.ReadAll(f)
 	if err != nil {
-		panic(err)
+		return "", err
 	}
 
-	data := string(b)
+	textFile := string(b)
 
-	return data
+	return textFile, nil
 }
 
-func WriteNewFile(data string, path string) {
-	f, err := os.Create(path)
+func OverwriteText(textFile string, path string) error {
+	f, err := os.OpenFile(path, os.O_RDWR|os.O_CREATE|os.O_TRUNC, 0755)
 	if err != nil {
-		panic(err)
+		return err
 	}
 
 	defer f.Close()
 
-	f.WriteString(data)
+	_, err = f.WriteString(textFile)
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
