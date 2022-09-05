616a9d5181e941dce1dc6ccd89355f44685dd592
afistapratama12 <pratama.otori.12@gmail.com>
2022-09-05 09:53:57 +0700

    feat: add main

--- tools/replace-to-EYD/main.go
+++ tools/replace-to-EYD/main.go
@@ -14,8 +14,8 @@ type ReplacerWord struct {
 	Replacer string
 }
 
-var listReplacerWord = []ReplacerWord{{"disini", "di sini"}, {"disana", "di sana"}, {"disitu", "di situ"}, {"didalam", "di dalam"}, {"diatas", "di atas"}, {"dibawah", "di bawah"},
-	{"Disini", "Di sini"}, {"Disana", "Di sana"}, {"Disitu", "Di situ"}, {"Didalam", "Di dalam"}, {"Diatas", "Di atas"}, {"Dibawah", "Di bawah"},
+var listReplacerWord = []ReplacerWord{{"diantara", "di antara"}, {"diakhir", "di akhir"}, {"diatas", "di atas"}, {"diawal", "di awal"}, {"dibawah", "di bawah"}, {"dibelakang", "di belakang"}, {"didalam", "di dalam"}, {"didekat", "di dekat"}, {"didepan", "di depan"}, {"dikanan", "di kanan"}, {"dikiri", "di kiri"}, {"diluar", "di luar"}, {"dimana", "di mana"}, {"disamping", "di samping"}, {"disaat", "di saat"}, {"disana", "di sana"}, {"disebelah", "di sebelah"}, {"disini", "di sini"}, {"disisi", "di sisi"}, {"disitu", "di situ"}, {"ditengah", "di tengah"}, {"ditengah-tengah", "di tengah-tengah"}, {"ditiap", "di tiap"}, {"ditiap-tiap", "di tiap-tiap"},
+	{"Diantara", "Di antara"}, {"Diakhir", "Di akhir"}, {"Diatas", "Di atas"}, {"Diawal", "Di awal"}, {"Dibawah", "Di bawah"}, {"Dibelakang", "Di belakang"}, {"Didalam", "Di dalam"}, {"Didekat", "Di dekat"}, {"Didepan", "Di depan"}, {"Dikanan", "Di kanan"}, {"Dikiri", "Di kiri"}, {"Diluar", "Di luar"}, {"Dimana", "Di mana"}, {"Disamping", "Di samping"}, {"Disaat", "Di saat"}, {"Disana", "Di sana"}, {"Disebelah", "Di sebelah"}, {"Disini", "Di sini"}, {"Disisi", "Di sisi"}, {"Disitu", "Di situ"}, {"Ditengah", "Di tengah"}, {"Ditengah-tengah", "Di tengah-tengah"}, {"Ditiap", "Di tiap"}, {"Ditiap-tiap", "Di tiap-tiap"},
 }
 
 func main() {
@@ -29,9 +29,17 @@ func main() {
 
 	for _, fileName := range strings.Split(listFileName, "\n") {
 		if fileName != "" && filepath.Ext(fileName) == ".md" {
-			MDText := OpenFile(fileName)
+			MDText, err := OpenFile(fileName)
+			if err != nil {
+				log.Println("Error open file", err.Error())
+				panic(err)
+			}
 			newMDText := ReplaceToEYD(MDText)
-			OverwriteText(newMDText, fileName)
+			err = OverwriteText(newMDText, fileName)
+			if err != nil {
+				log.Println("Error Overwrite text file", err.Error())
+				panic(err)
+			}
 		}
 	}
 }
@@ -44,39 +52,36 @@ func ReplaceToEYD(textFile string) string {
 	return textFile
 }
 
-func OpenFile(path string) string {
+func OpenFile(path string) (string, error) {
 	f, err := os.Open(path)
 	if err != nil {
-		log.Println("Error open file", err.Error())
-		panic(err)
+		return "", err
 	}
 
 	defer f.Close()
 
 	b, err := ioutil.ReadAll(f)
 	if err != nil {
-		log.Println("Error ioutil Readall", err.Error())
-		panic(err)
+		return "", err
 	}
 
 	textFile := string(b)
 
-	return textFile
+	return textFile, nil
 }
 
-func OverwriteText(textFile string, path string) {
+func OverwriteText(textFile string, path string) error {
 	f, err := os.OpenFile(path, os.O_RDWR|os.O_CREATE|os.O_TRUNC, 0755)
 	if err != nil {
-		log.Println("Error open file", err.Error())
-		panic(err)
+		return err
 	}
 
 	defer f.Close()
 
 	_, err = f.WriteString(textFile)
 	if err != nil {
-		log.Println("Error write new text to file", err.Error())
-		panic(err)
+		return err
 	}
-	return
+
+	return nil
 }
